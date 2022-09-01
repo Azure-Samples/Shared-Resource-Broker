1825e394819ce46234d6a2968a4387a4e9e22c29
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-09-01 11:50:24 +0200

    Currently not using nullable

--- src/Backend/Models/Request/SubscriptionRegistrationRequest.cs
+++ src/Backend/Models/Request/SubscriptionRegistrationRequest.cs
@@ -2,7 +2,7 @@
 
 public record SubscriptionRegistrationRequest(string SubscriptionID, string ResourceGroupName)
 {
-    public string? Secret { get; init; }
-    public string? Resourcegroup { get; init; }
-    public string? Subscription { get; init; }
+    public string Secret { get; init; }
+    public string Resourcegroup { get; init; }
+    public string Subscription { get; init; }
 }
